f43d82853bf368936532d49ed12f26a38c8b4f50
Vondráček <Stepan.Vondracek@telekom.com>
2023-03-21 09:53:18 +0100

    adding helper functions

--- utils/help.py
+++ utils/help.py
@@ -1,14 +1,9 @@
-from abc import abstractmethod
-
-import pandas as pd
 import os
+import pandas as pd
+import seaborn as sns
 
 
-class Help:
-    def __init__(self):
-        pass
-
-def read_csv(self, path: str) -> dict:
+def read_csv(path: str) -> dict:
     """
     Loads all .csv files present in path into a dictionary
     """
@@ -19,7 +14,8 @@ def read_csv(self, path: str) -> dict:
             csv_dict[prefix] = pd.read_csv(os.path.join(path, file))
     return csv_dict
 
-def read_csv_v2(self, path: str) -> dict:
+
+def read_csv_v2(path: str) -> dict:
     """
     Just like read_csv but removes padding
     """
@@ -30,7 +26,8 @@ def read_csv_v2(self, path: str) -> dict:
             csv_dict[str(int(prefix))] = pd.read_csv(os.path.join(path, file))
     return csv_dict
 
-def read_csv_v3(self, path: str) -> dict:
+
+def read_csv_v3(path: str) -> dict:
     """
     Removes padding using another function
     """
@@ -38,59 +35,59 @@ def read_csv_v3(self, path: str) -> dict:
     for file in os.listdir(path):
         prefix, suffix = file.split(".")
         if suffix == "csv":
-            csv_dict[self.remove_padding(prefix)] = pd.read_csv(os.path.join(path, file))
+            csv_dict[remove_padding(prefix)] = pd.read_csv(os.path.join(path, file))
     return csv_dict
 
-def read_csv_v4(self, path: str, file_subset=None) -> dict:
+
+def read_csv_v4(path: str, id_list=None) -> dict:
     """
     Makes it possible to load only a subset of the data
     """
     csv_dict = {}
+    files_listed = [f"{station.zfill(3)}.csv" for station in id_list]
     for file in os.listdir(path):
         prefix, suffix = file.split(".")
         if suffix == "csv":
-            if file_subset is None or file in file_subset:
-                csv_dict[self.remove_padding(prefix)] = pd.read_csv(os.path.join(path, file))
+            if id_list is None or file in files_listed:
+                csv_dict[remove_padding(prefix)] = pd.read_csv(os.path.join(path, file))
     return csv_dict
 
-def test_read_csv(self, path):
+
+def test_read_csv(path):
     """
     Test for the read_csv method
     """
-    dict_full = self.read_csv(path)
+    dict_full = read_csv(path)
     assert len(dict_full) == len([f for f in os.listdir(path) if ".csv" in f])
     print(f"Tests passed for the test_read_csv({path})")
 
-def remove_padding(self, tgt_string: str) -> str:
+
+def remove_padding(tgt_string: str) -> str:
     """
     Removes the padding from the file
     """
     str_out = str(int(tgt_string))
     return str_out
 
-def test_remove_padding(self, test_cases=['0125', '1250']):
-    assert self.remove_padding(test_cases[0] == '125')
-    print(f"remove_pading({test_cases[0]}) -> {self.remove_padding(test_cases[0])} Test OK")
-    assert self.remove_padding(test_cases[1] == '1250')
-    print(f"remove_pading({test_cases[1]}) -> {self.remove_padding(test_cases[1])} Test OK")
 
-def get_minmax_date(self, df):
+def test_remove_padding(test_cases=['0125', '1250']):
+    assert remove_padding(test_cases[0] == '125')
+    print(f"remove_pading({test_cases[0]}) -> {remove_padding(test_cases[0])} Test OK")
+    assert remove_padding(test_cases[1] == '1250')
+    print(f"remove_pading({test_cases[1]}) -> {remove_padding(test_cases[1])} Test OK")
 
-    df['Date'] = pd.to_datetime(df['Date'])
-    min_d, max_d = (min(df['Date']), max(df['Date']))
 
-    df_out = pd.DataFrame({'id': [df["ID"].values[0]],
-                           'min_d': [min_d],
-                           'max_d': [max_d],
-                           'days_diff': [(max_d - min_d).days]})
-    return df_out
+def plot_monitoring_station(df, id_vars=['Date', 'ID'], value_vars=['sulfate', 'nitrate']):
+    data_plotted = df.melt(id_vars=id_vars, value_vars=value_vars)
+    g = sns.FacetGrid(data_plotted, col='ID', hue='variable', legend_out=True)
+    g.map(sns.lineplot, 'Date', 'value')
 
-def concat_date_ranges(self, dfs_all, subset=None):
 
-    date_ranges = []
-
-    for k, v in dfs_all.items():
-        if subset == None or k in subset:
-            date_ranges.append(self.get_minmax_date(v))
-
-    return pd.concat(date_ranges).sort_values('id')
+def get_monitor(station_ids: list, dropna=True, plot=True):
+    stations_data = read_csv_v4("./specdata", station_ids)
+    stations_data = pd.concat(stations_data.values())
+    if dropna:
+        stations_data = stations_data.dropna()
+    if plot:
+        plot_monitoring_station(stations_data)
+    return stations_data
